924e9f4d80798448a91eaa439a1f05c4e69fac67
Raymond Zhang <zhang.rayz@gmail.com>
2016-07-23 17:47:42 -0700

    started implementing unlock form

--- lib/distraction.jsx
+++ lib/distraction.jsx
@@ -3,7 +3,9 @@ const React = require('react');
 const DistractCat = React.createClass({
   getInitialState() {
     return (
-      {offRequests: 0}
+			{offRequests: 0,
+				random: '',
+			showForm: false}
     );
 	},
 	componentDidMount () {
@@ -14,14 +16,44 @@ const DistractCat = React.createClass({
 			this.setState({offRequests: storage['offRequests']});
 		});
 	},
+	toggleForm () {
+		if (this.state.showForm) {
+			this.setState({showForm: false, random: ''});
+		} else {
+			let string = stringGen();
+			this.setState({showForm: true, random: string});
+		}
+	},
   render () {
-    return (
+		let main = "main-container";
+		if (this.state.offRequests >= 2) {
+			main = "main-container weather rain";
+		}
+		let rightTear = (
+			<img src="./assets/teardrop.png" className="right-tear"/>
+		);
+		let leftTear = (
+			<img src="./assets/small_teardrop.png" className="left-tear"/>
+		);
+		let theForm = (
 			<div>
+				<div>Kitty will become very sad...</div>
+				<div>If you are sure, please enter the unlock code below:</div>
+				<div className="noSelect">{this.state.random}</div>
+				<input type="text" className="unlock-input"/>
+			</div>
+		);
+    return (
+			<div className={main}>
 				<div className="cat-container">
 					<img src="./assets/sad_cat.jpg" alt="sad big eyed cat" className="cat-pic"/>
 					<h2>Why You No Do Work?</h2>
-					<img src="./assets/teardrop.png" className="big-tear"/>
-          <img src="./assets/small_teardrop.png" className="left-tear"/>
+					{this.state.offRequests >= 1 ? rightTear : null}
+					{this.state.offRequests >= 1 ? leftTear : null}
+					<div className="form-container">
+						<label onClick={this.toggleForm}>Want to disable Distract No Mo?</label>
+						{this.state.showForm ? theForm : null}
+					</div>
 				</div>
 			</div>
     );
@@ -29,3 +61,14 @@ const DistractCat = React.createClass({
 });
 
 module.exports = DistractCat;
+
+const stringGen = function () {
+	let text = "";
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+	for (let i = 0; i < 25; i++) {
+		text += charset.charAt(Math.floor(Math.random() * charset.length));
+	}
+
+	return text;
+}
